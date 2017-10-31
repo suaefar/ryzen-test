05dc4439977fd46711dc17541a1ea2f9bfa897de
Marc René Schädler <suaefar@googlemail.com>
2017-10-31 12:59:48 +0100

    Revert "Fixes related to gcc-7.1 compilation errors and further usability improvements"
    This reverts commit b28e6d7d7fd881484e2a640e7911a99e403d9695.

--- kill-ryzen.sh
+++ kill-ryzen.sh
@@ -5,7 +5,6 @@ USE_RAMDISK=true
 CLEAN_ON_EXIT=false
 NPROC=$1
 TPROC=$2
-MOUNTPOINT="/mnt/ramdisk"
 
 [ -n "$NPROC" ] || NPROC=$(nproc)
 [ -n "$TPROC" ] || TPROC=1
@@ -24,42 +23,34 @@ if which apt-get &>/dev/null; then
 elif which dnf &>/dev/null; then
   sudo dnf install -y @development-tools
 elif which pacman &>/dev/null; then
-  echo "You are using a rolling release distribution based on ArchLinux. You are on your own if things fail."
   sudo pacman -S --needed base-devel
 else
-  echo "Your distribution is not officially supported. The script will continue but may fail if you don't have the required build tools. You are on your own if things fail."
+  exit 1
 fi
 
 if $USE_RAMDISK; then
-  #Create compressed ramdisk if it doesn't already exist
-  if mountpoint -q $MOUNTPOINT; then
-    echo "RAMdisk already mounted on $MOUNTPOINT."
-  else
-    echo "Create compressed ramdisk"
-    sudo mkdir -p $MOUNTPOINT || exit 1
-    sudo modprobe zram num_devices=1 || exit 1
-    # Setting maximum RAM disk size
-    echo 64G | sudo tee /sys/block/zram0/disksize || exit 1
-    sudo mkfs.ext4 -q -m 0 -b 4096 -O sparse_super -L zram /dev/zram0 || exit 1
-    sudo mount -o relatime,nosuid,discard /dev/zram0 $MOUNTPOINT || exit 1
-  fi
-  sudo mkdir -p $MOUNTPOINT/workdir || exit 1
-  sudo chmod 777 $MOUNTPOINT/workdir || exit 1
-  cp buildloop.sh $MOUNTPOINT/workdir/buildloop.sh || exit 1
-  cd $MOUNTPOINT/workdir || exit 1
-  mkdir -p tmpdir || exit 1
+  echo "Create compressed ramdisk"
+  sudo mkdir -p /mnt/ramdisk || exit 1
+  sudo modprobe zram num_devices=1 || exit 1
+  echo 64G | sudo tee /sys/block/zram0/disksize || exit 1
+  sudo mkfs.ext4 -q -m 0 -b 4096 -O sparse_super -L zram /dev/zram0 || exit 1
+  sudo mount -o relatime,nosuid,discard /dev/zram0 /mnt/ramdisk/ || exit 1
+  sudo mkdir -p /mnt/ramdisk/workdir || exit 1
+  sudo chmod 777 /mnt/ramdisk/workdir || exit 1
+  cp buildloop.sh /mnt/ramdisk/workdir/buildloop.sh || exit 1
+  cd /mnt/ramdisk/workdir || exit 1
+  mkdir tmpdir || exit 1
   export TMPDIR="$PWD/tmpdir"
 fi
 
 echo "Download GCC sources"
-DOWNLOAD_FILE=gcc-7.2.0.tar.xz
-wget -O "$DOWNLOAD_FILE" -c ftp://ftp.fu-berlin.de/unix/languages/gcc/releases/gcc-7.2.0/$DOWNLOAD_FILE || exit 1
+wget ftp://ftp.fu-berlin.de/unix/languages/gcc/releases/gcc-7.1.0/gcc-7.1.0.tar.bz2 || exit 1
 
 echo "Extract GCC sources"
-tar xf $DOWNLOAD_FILE || exit 1
+tar xf gcc-7.1.0.tar.bz2 || exit 1
 
 echo "Download prerequisites"
-(cd gcc-7.2.0/ && ./contrib/download_prerequisites)
+(cd gcc-7.1.0/ && ./contrib/download_prerequisites)
 
 [ -d 'buildloop.d' ] && rm -r 'buildloop.d'
 mkdir -p buildloop.d || exit 1
